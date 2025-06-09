183b026441cacf24e6b22cd5028429074ae76f37
Chet <cmcgrury2@huskers.unl.edu>
2025-06-09 17:37:19 -0500

    Added weather alerts functionality for specific places/people, sunrise/sunset to !weather, !alert command

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import requests
 import json
 import discord
 from discord import Intents
+from discord.ext import tasks
+from datetime import datetime
 
 
 load_dotenv() # Gets the data from my .env file where the api keys are stored
@@ -33,20 +35,28 @@ client = discord.Client(intents=intents) # needs the intents passed into the cli
 @client.event
 async def on_ready(): # Must be async because discord bots need to run this way because they are constantly doing things like listening for messages, responding to events, etc. Async lets the program start a task, wait for it to finish, and do something elsew while it's waiting.
     print(f"Logged in as Roeland Weather Bot")
+    check_weather_alerts.start()
 
 @client.event
 async def on_message(message):
     if message.author == client.user: # returns nothing if the message author is the bot
         return
     
+    allowedChannelId = {1381749910247964761, 1381417910244741241}
+    if message.channel.id not in allowedChannelId:
+        return
+
     if message.content.startswith("!weather"):
         location = message.content[len("!weather"):].strip() # slices off "!weather" from the start of the message, leaving just the location (e.g., "Beatrice, NE"). The colon means "start at index 8 and go to the end"
         
         url = f"https://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q={location}"
+        urlSun = f"https://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q={location}"
 
         try:
             response = requests.get(url)
+            responseSun = requests.get(urlSun)
             data = response.json()
+            dataSun = responseSun.json()
         except requests.exceptions.RequestException:
             await message.channel.send("Couldn't connect to the weather service. Try again later.")
             return
@@ -55,6 +65,8 @@ async def on_message(message):
             return
 
         # await message.channel.send(data["location"]["name"], data["current"]["temp_f"], data["current"]["condition"]["text"]) ### This doesn't work because .send() can't accept more than a single argument.
+
+        
 
         condition_emojis = { # dictionary to house all the emojis
             "sunny": "☀️",
@@ -97,6 +109,8 @@ async def on_message(message):
             humidity = data["current"]["humidity"]
             uv = data["current"]["uv"]
             precip = data["current"]["precip_in"]
+            sunrise = dataSun["forecast"]["forecastday"][0]["astro"]["sunrise"]
+            sunset = dataSun["forecast"]["forecastday"][0]["astro"]["sunset"]
 
 
             emoji = "❔"
@@ -114,7 +128,8 @@ async def on_message(message):
 
         except KeyError:
             await message.channel.send("Couldn't find weather for that location. Make sure it's valid, like `Beatrice, NE` or `Lincoln`")
-
+            return
+        
         await message.channel.send(
             f"# Weather in {city}\n"
             f"{emoji} Condition: {condition}\n"
@@ -126,7 +141,9 @@ async def on_message(message):
             f"💨 Wind: {windSpeed} mph from {windDirection}\n"
             f"💨💨 Gusts: {gust} mph\n"
             f"☀️ UV Level: {uvLevel}, {uv}\n"
-            f"⏲️ Last updated: {lastUpdated} (local time)"
+            f"🌅 Sunrise: {sunrise} (location time)\n"
+            f"🌇 Sunset: {sunset} (location time)\n"
+            f"⏲️ Last updated: {lastUpdated} (location time)"
 
                                    ) # sends the current weather in a message
         try:
@@ -134,5 +151,117 @@ async def on_message(message):
         except discord.Forbidden:
             print("Bot doesn't hav epermission to delete messages.")
         # print(f"Getting weather for {location}")
+
+    if message.content.startswith("!alert"):
+        locationAlert = message.content[len("!alert"):].strip()
+        urlAlerts = f"https://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q={locationAlert}&alerts=yes"
+
+        try:
+            response = requests.get(urlAlerts)
+            dataAlerts = response.json()
+        except requests.exceptions.RequestException:
+            await message.channel.send("Couldn't connect to the weather service. Try again later.")
+            return
+        except ValueError:
+            await message.channel.send("The response wasn't valid JSON")
+            return
+        
+        alerts = dataAlerts.get("alerts", {}).get("alert", [])
+
+        if not alerts:
+           await message.channel.send(f"No active weather alerts for **{locationAlert}**")
+
+        for alert in alerts:
+            headline = alert.get("headline", "No Headline")
+            severity = alert.get("severity", "Unknown")
+            urgency = alert.get("urgency", "Unknown")
+            areas = alert.get("areas", "")
+            certainty = alert.get("certainty", "")
+            event = alert.get("event", "")
+            start = alert.get("effective", "")
+            end = alert.get("expires", "")
+            desc = alert.get("desc", "")
+            instruction = alert.get("instruction", "")
+
+            alertMessage = (
+                f"**{headline}** ({severity})\n"
+                f"**Urgency:** {urgency}\n"
+                f"**Certainty:** {certainty}\n"
+                f"\n"
+                f"\n"
+                f"{desc}\n"
+                f"**Instructions:** {instruction}"
+            )
+
+            await message.channel.send(alertMessage)
+
+        try:
+            await message.delete() # delete command
+        except discord.Forbidden:
+            print("Bot doesn't hav epermission to delete messages.")
+
+    
+
+
+seenAlerts = set()
+
+locationAlertTags = {
+    "Beatrice, NE": [320361266407276546],
+    "Lincoln, NE": [456265293099040768],
+    "Los Angeles, CA": [190277036600721408]
+}
+
+@tasks.loop(minutes=1)
+async def check_weather_alerts():
+    allowedChannelIds = [1381417910244741241, 1381749910247964761]
+    allowedChannels = [client.get_channel(ch_id) for ch_id in allowedChannelIds]
+    locations = ["Beatrice, NE", "Omaha, NE", "Lincoln, NE", "Los Angeles, CA", "Kearney, NE"]
+
+    for location in locations:
+        userIds = locationAlertTags.get(location, [])
+        tag = " ".join(f"<@{uid}>" for uid in userIds)
+        url = f"http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q={location}&alerts=yes"
+
+        response = requests.get(url)
+        data = response.json()
+
+        alerts = data.get("alerts", {}).get("alert", [])
+        if alerts:
+            print("Alerts found")
+        else:
+            print("No alerts found")
+
+        for alert in alerts:
+            f"{tag}\n"
+            headline = alert.get("headline", "No Headline")
+            severity = alert.get("severity", "Unknown")
+            urgency = alert.get("urgency", "Unknown")
+            areas = alert.get("areas", "")
+            certainty = alert.get("certainty", "")
+            event = alert.get("event", "")
+            start = alert.get("effective", "")
+            end = alert.get("expires", "")
+            desc = alert.get("desc", "")
+            instruction = alert.get("instruction", "")
+
+            alertMessage = (
+                f"**{headline}** ({severity})\n"
+                f"**Urgency:** {urgency}\n"
+                f"**Certainty:** {certainty}\n"
+                f"\n"
+                f"\n"
+                f"{desc}\n"
+                f"**Instructions:** {instruction}"
+            )
+
+            if headline not in seenAlerts:
+                seenAlerts.add(headline)
+
+                for channel in allowedChannels:
+                    if channel:
+                        await allowedChannels.send(alertMessage)
+
+
+
 
 client.run(BOT_TOKEN) # connect to discord's servers, log in using bot token, start asynchronous event loop that listens for on_ready, on_message, on_member_join, keeps the bot running. uses the on_ready() function I made.
